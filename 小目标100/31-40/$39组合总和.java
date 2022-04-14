12a0118323431f34f20628c7a32af9ec67d4a07b
yyh2433152702 <2433152702@qq.com>
2022-04-14 18:32:37 +0800

    39

--- 小目标100/31-40/$39组合总和.java
+++ 小目标100/31-40/$39组合总和.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -11,51 +10,45 @@ import java.util.stream.Collectors;
  * 仅有这两种组合。
  */
 public class $39组合总和 {
-
+    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < candidates.length; i++) {
-            ArrayList<Integer> now = new ArrayList<>();
-            now.add(candidates[i]);
-            dfs(res, candidates, target, i, now, candidates[i]);
+        dfs(candidates, target, 0, new ArrayList<>(), 0);
+        Set<List<Integer>> temp = new HashSet<>();
+        for (List<Integer> r: res) {
+            temp.add(r.stream().sorted().collect(Collectors.toList()));
         }
-        return res.stream().distinct().collect(Collectors.toList());
+        res.clear();
+        res.addAll(temp);
+        return res;
     }
 
     /**
      * 递归+剪枝
-     * @param res 最终结果集
      * @param candidates 整数列表
      * @param target 目标值
      * @param index 当前分支最小值位置
      * @param now 当前整数组合
      * @param sum 当前整数组合值
      */
-    private void dfs(List<List<Integer>> res, int[] candidates, int target, int index, List<Integer> now, int sum) {
-        for (int i = index; i < candidates.length; i++) {
-            sum += candidates[i];
-            now.add(candidates[i]);
-            if (target<sum) {
-                if (now.size()>0) {
-                    now.remove(candidates[i]);
-                    sum -= candidates[i];
+    private void dfs(int[] candidates, int target, int index, List<Integer> now, int sum) {
+        if (target==sum) {
+            res.add(new ArrayList<>(now));
+        }else{
+            for (int i = 0; i < candidates.length; i++) {
+                sum += candidates[i];
+                now.add(candidates[i]);
+                if (target>=sum) {
+                    dfs(candidates, target, i, now, sum);
                 }
+                now.remove(now.size()-1);
+                sum -= candidates[i];
             }
-            if (target==sum) {
-                res.add(now);
-                break;
-            }
-            if (target>sum) {
-                dfs(res, candidates, target, i, now, sum);
-            }
-
-
         }
     }
 
     public static void main(String[] args) {
-        int[] candidates = {2,3,6,7};
-        int target = 7;
+        int[] candidates = {2};
+        int target = 1;
         new $39组合总和().combinationSum(candidates, target).forEach(System.out::println);
     }
 }
