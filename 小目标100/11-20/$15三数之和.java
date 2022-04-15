33bd613d3403a20649a039f6ed5c8b5281a237f5
yyh2433152702 <2433152702@qq.com>
2022-04-15 11:30:50 +0800

    15

--- 小目标100/11-20/$15三数之和.java
+++ 小目标100/11-20/$15三数之和.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 输入：nums = [-1,0,1,2,-1,-4]
@@ -8,12 +10,47 @@ import java.util.List;
 public class $15三数之和 {
 
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-
-        return res;
+        List<List<Integer>> lists = new ArrayList<>();
+        //排序
+        Arrays.sort(nums);
+        //双指针
+        int len = nums.length;
+        for(int i = 0;i < len;++i) {
+            if(nums[i] > 0) {
+                return lists;
+            }
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int curr = nums[i];
+            int L = i+1, R = len-1;
+            while (L < R) {
+                int tmp = curr + nums[L] + nums[R];
+                if(tmp == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(curr);
+                    list.add(nums[L]);
+                    list.add(nums[R]);
+                    lists.add(list);
+                    while(L < R && nums[L+1] == nums[L]) {
+                        ++L;
+                    }
+                    while (L < R && nums[R-1] == nums[R]) {
+                        --R;
+                    }
+                    ++L;
+                    --R;
+                } else if(tmp < 0) {
+                    ++L;
+                } else {
+                    --R;
+                }
+            }
+        }
+        return lists;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(new $15三数之和().threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
     }
 }
