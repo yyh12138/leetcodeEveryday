a46a60bb5fb6ad6d4d0917bc90ef189b76a12af1
yyh2433152702 <2433152702@qq.com>
2022-04-27 10:55:49 +0800

    417

---
+++ 中目标500/401-500/$417太平洋水流问题.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+/**
+ * 有一个 m × n 的矩形岛屿，与 太平洋 和 大西洋 相邻。 “太平洋” 处于大陆的左边界和上边界，而 “大西洋” 处于大陆的右边界和下边界。
+ * 这个岛被分割成一个由若干方形单元格组成的网格。给定一个 m x n 的整数矩阵 heights ， heights[r][c] 表示坐标 (r, c) 上单元格 高于海平面的高度 。
+ * 岛上雨水较多，如果相邻单元格的高度 小于或等于 当前单元格的高度，雨水可以直接向北、南、东、西流向相邻单元格。水可以从海洋附近的任何单元格流入海洋。
+ * 返回 网格坐标 result 的 2D列表 ，其中 result[i] = [ri, ci] 表示雨水可以从单元格 (ri, ci) 流向 太平洋和大西洋 。
+ *
+ * 输入: heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+ * 输出: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+ */
+public class $417太平洋水流问题 {
+
+    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int[][] heights;
+    int m, n;
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        this.heights = heights;
+        this.m = heights.length;
+        this.n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            dfs(i, 0, pacific); // bfs
+        }
+        for (int j = 1; j < n; j++) {
+            dfs(0, j, pacific); // bfs
+        }
+        for (int i = 0; i < m; i++) {
+            dfs(i, n - 1, atlantic); // bfs
+        }
+        for (int j = 0; j < n - 1; j++) {
+            dfs(m - 1, j, atlantic); // bfs
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    List<Integer> cell = new ArrayList<>();
+                    cell.add(i);
+                    cell.add(j);
+                    result.add(cell);
+                }
+            }
+        }
+        return result;
+    }
+
+    public void dfs(int row, int col, boolean[][] ocean) {
+        if (ocean[row][col]) {
+            return;
+        }
+        ocean[row][col] = true;
+        for (int[] dir : dirs) {
+            int newRow = row + dir[0], newCol = col + dir[1];
+            if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && heights[newRow][newCol] >= heights[row][col]) {
+                dfs(newRow, newCol, ocean);
+            }
+        }
+    }
+
+    public void bfs(int row, int col, boolean[][] ocean) {
+        if (ocean[row][col]) {
+            return;
+        }
+        ocean[row][col] = true;
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{row, col});
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] dir : dirs) {
+                int newRow = cell[0] + dir[0], newCol = cell[1] + dir[1];
+                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && heights[newRow][newCol] >= heights[cell[0]][cell[1]] && !ocean[newRow][newCol]) {
+                    ocean[newRow][newCol] = true;
+                    queue.offer(new int[]{newRow, newCol});
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] heights = {
+                            {1,2,2,3,5},
+                            {3,2,3,4,4},
+                            {2,4,5,3,1},
+                            {6,7,1,4,5},
+                            {5,1,1,2,4}
+                            };
+        List<List<Integer>> res = new $417太平洋水流问题().pacificAtlantic(heights);
+        for (List<Integer> r : res) {
+            for (int rr :
+                    r) {
+                System.out.print(rr+" ");
+            }
+            System.out.println();
+        }
+    }
+}
