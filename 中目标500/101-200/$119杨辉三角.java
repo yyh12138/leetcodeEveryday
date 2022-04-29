27a4bc09f0803af4f8b5b153ed7ab422e415b239
yyh2433152702 <2433152702@qq.com>
2022-04-29 10:09:42 +0800

    427

---
+++ 中目标500/101-200/$119杨辉三角.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+/**
+ * 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+ * 输入: rowIndex = 3
+ * 输出: [1,3,3,1]
+ */
+public class $119杨辉三角 {
+
+    public List<Integer> getRow(int rowIndex) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new $119杨辉三角().getRow(3));
+    }
+}
