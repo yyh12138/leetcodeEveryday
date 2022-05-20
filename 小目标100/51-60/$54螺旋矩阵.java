bc0adbec70e420486aa5cb2d404504dbcea41c50
yyh2433152702 <2433152702@qq.com>
2022-05-20 10:18:18 +0800

    54

--- 小目标100/51-60/$54螺旋矩阵.java
+++ 小目标100/51-60/$54螺旋矩阵.java
@@ -12,11 +12,6 @@ import java.util.List;
  */
 public class $54螺旋矩阵 {
 
-    final static int RIGHT = 0;
-    final static int DOWN = 1;
-    final static int LEFT = 2;
-    final static int UP = 3;
-
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
@@ -26,19 +21,31 @@ public class $54螺旋矩阵 {
         int left = 0, right = columns - 1, top = 0, bottom = rows - 1;
 
         while (left<=right && top<=bottom) {
-
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            for (int i = top+1; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            if (left<right && top<bottom) {
+                for (int i = right-1; i > left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                for (int i = bottom; i > top; i--) {
+                    res.add(matrix[i][left]);
+                }
+            }
             left++;
             right--;
             top++;
             bottom--;
         }
-
         return res;
     }
 
     public static void main(String[] args) {
         System.out.println(new $54螺旋矩阵().spiralOrder(new int[][]{
-                {1,2,3,4}, {5,6,7,8}, {9,10,11,12}
+                {1,2,3}, {4,5,6}, {7,8,9}
         }));
     }
 }
