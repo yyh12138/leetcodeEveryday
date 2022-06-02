8732b847ae490b656721820b0df45e8b6f08e73d
yyh2433152702 <2433152702@qq.com>
2022-06-02 09:56:40 +0800

    450

---
+++ 中目标500/401-500/$450删除二叉搜索树中节点.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 输入：root = [5,3,6,2,4,null,7], key = 3
+ * 输出：[5,4,6,2,null,null,7]
+ * 解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+ * 一个正确的答案是 [5,4,6,2,null,null,7],
+ * 另一个正确答案是 [5,2,6,null,4,null,7]。
+ *
+ */
+public class $450删除二叉搜索树中节点 {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        TreeNode cur = root, curParent = null;
+        while (cur != null && cur.val != key) {
+            curParent = cur;
+            if (cur.val > key) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        if (cur == null) {
+            return root;
+        }
+        if (cur.left == null && cur.right == null) {
+            cur = null;
+        } else if (cur.right == null) {
+            cur = cur.left;
+        } else if (cur.left == null) {
+            cur = cur.right;
+        } else {
+            TreeNode successor = cur.right, successorParent = cur;
+            while (successor.left != null) {
+                successorParent = successor;
+                successor = successor.left;
+            }
+            if (successorParent.val == cur.val) {
+                successorParent.right = successor.right;
+            } else {
+                successorParent.left = successor.right;
+            }
+            successor.right = cur.right;
+            successor.left = cur.left;
+            cur = successor;
+        }
+        if (curParent == null) {
+            return cur;
+        } else {
+            if (curParent.left != null && curParent.left.val == key) {
+                curParent.left = cur;
+            } else {
+                curParent.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(50,new TreeNode(30,null,new TreeNode(40)),
+                                    new TreeNode(70,new TreeNode(60),new TreeNode(80)));
+        new $450删除二叉搜索树中节点().deleteNode(root, 50);
+    }
+}
