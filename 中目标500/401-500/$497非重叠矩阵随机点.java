6a39208860dcd2b3b7b32a2dab53fcd9f94055e3
yyh2433152702 <2433152702@qq.com>
2022-06-09 09:32:54 +0800

    497

---
+++ 中目标500/401-500/$497非重叠矩阵随机点.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+/**
+ * 设计一个算法来随机挑选一个被某一矩形覆盖的整数点。矩形周长上的点也算做是被矩形覆盖。所有满足要求的点必须等概率被返回。
+ *
+ * 输入:
+ * ["Solution","pick","pick","pick","pick","pick"]
+ * [[[[-2,-2,-1,-1],[1,0,3,0]]],[],[],[],[],[]]
+ * 输出:
+ * [null,[-1,-2],[2,0],[-2,-1],[3,0],[-2,-2]
+ *
+ * 解释：
+ * Solution solution = new Solution([[-2, -2, 1, 1], [2, 2, 4, 6]]);
+ * solution.pick(); // 返回 [1, -2]
+ * solution.pick(); // 返回 [1, -1]
+ * solution.pick(); // 返回 [-1, -2]
+ * solution.pick(); // 返回 [-2, -2]
+ * solution.pick(); // 返回 [0, 0]
+ */
+public class $497非重叠矩阵随机点 {
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(new int[][]{
+                {-2,-2,1,1}, {2,2,4,6}
+        });
+        System.out.println(Arrays.toString(rectangle.pick()));
+    }
+}
+
+class Rectangle {
+
+    Random random;
+    List<Integer> arr;
+    int[][] rects;
+
+    public Rectangle(int[][] rects) {
+        random = new Random();
+        arr = new ArrayList<>();
+        arr.add(0);
+        this.rects = rects;
+        for (int[] rect : rects) {
+            int a = rect[0], b = rect[1], x = rect[2], y = rect[3];
+            arr.add(arr.get(arr.size()-1) + (x-a+1)*(y-b+1));
+        }
+    }
+
+    public int[] pick() {
+        int k = random.nextInt(arr.get(arr.size()-1));
+        int rectIndex = binarySearch(arr, k + 1)-1;
+        k -= arr.get(rectIndex);
+        int[] rect = rects[rectIndex];
+        int a = rect[0], b = rect[1], y = rect[3];
+        int col = y - b + 1;
+        int da = k / col;
+        int db = k - col * da;
+        return new int[]{a + da, b + db};
+    }
+
+    private int binarySearch(List<Integer> arr, int target) {
+        int low = 0, high = arr.size() - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = arr.get(mid);
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
