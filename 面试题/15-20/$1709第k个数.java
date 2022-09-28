e222c9f97cc015f531788cf9be5f8bf5b7a7c588
yyh2433152702 <2433152702@qq.com>
2022-09-28 10:10:22 +0800

    1709

--- 面试题/15-20/$1709第k个数.java
+++ 面试题/15-20/$1709第k个数.java
@@ -7,28 +7,19 @@ import java.util.List;
  */
 public class $1709第k个数 {
     // 1，3，5，7，9，15，21. 25, 27,
+    // 某个满足的数，一定是之前resultA*3或者是resultB*5或者是resultC*7 的结果，并且结果一定是这三个乘积的最小值
+    // 因此，只要能够记录resultA、resultB、resultC，再相互与3、5、7相乘，取其中的最小值，就是当前的目标值
     public int getKthMagicNumber(int k) {
-        int [] result = new int[k];
-        result[0] = 1;
-        // 某一个满足结果的数，一定是之前的某个 resultA*3 或者是 resultB*5 或者是 resultC*7 的结果
-        // 并且结果一定是这三个乘积的最小值，因此，只要能够记录resultA、resultB、resultC，再相互与 3、5、7 相乘，取其中的最小值，就是当前的目标值
-        int point3 = 0;
-        int point5 = 0;
-        int point7 = 0;
-        for (int i = 1; i < k; i++) {
-            int resultN = Math.min(Math.min(result[point3] * 3, result[point5] * 5), result[point7] * 7);
-            if (resultN % 3 == 0) {
-                point3++;
-            }
-            if (resultN % 5 == 0) {
-                point5++;
-            }
-            if (resultN % 7 == 0) {
-                point7++;
-            }
-            result[i] = resultN;
+        int[] dp = new int[k];
+        dp[0] = 1;// 第一个数是1
+        int p3 = 0,p5 = 0,p7 = 0;// 指向等待乘以3/5/7的dp下标
+        for(int i=1; i<k; i++){
+            dp[i] = Math.min(3*dp[p3], Math.min(5*dp[p5],7*dp[p7]));// 归并，选择最小的加入
+            if(dp[i] == 3*dp[p3]) { p3++; }
+            if(dp[i] == 5*dp[p5]) { p5++; }
+            if(dp[i] == 7*dp[p7]) { p7++; }
         }
-        return result[k - 1];
+        return dp[k-1];
     }
 
     public static void main(String[] args) {
